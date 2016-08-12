a40ee6cdc95c1cee5740df5aed67560f2d1a81a5
samdx@hongha.asia <samdx@hongha.asia>
2016-08-12 22:08:13 +0700

    search.js + peform the search.

--- assets/js/search.js
+++ assets/js/search.js
@@ -16,3 +16,35 @@ function getQueryVariable (variable) {
 
 var searchTerm = getQueryVariable ('query');
 
+/*
+## Peform the search
+*/
+
+if (searchTerm) {
+  document.getElementById('search-box').setAttribute("value", searchTerm);
+
+  // Initalize lunr with the fields it will be searching on. I've given title
+  // a bost of 10 to indeicate matches on this field are more important.
+
+  var idex_item = lunr(function () {
+    this.field('id');
+    this.field('title', { boost: 10 });
+    this.field('author');
+    this.field('category');
+    this.field('content');
+  });
+
+  for (var key in window.store) {
+    idex_item.add({
+      'id': key,
+      'title': window.store[key].title,
+      'author': window.store[key].author,
+      'category': window.store[key].category,
+      'content': window.store[key].content
+    });
+
+  var results = idex_item.search (searchTerm);
+  displaySearchResults (results, window.store);
+  }
+}
+
