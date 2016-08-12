723a9dbc137c94da9b3c426e030cd5873214400f
samdx@hongha.asia <samdx@hongha.asia>
2016-08-12 22:14:40 +0700

    Display the results.

--- assets/js/search.js
+++ assets/js/search.js
@@ -48,3 +48,25 @@ if (searchTerm) {
   }
 }
 
+/*
+## Display the results
+*/
+
+function displaySearchResults (results, store) {
+  var searchResults = document.getElementById ('search-results');
+
+  if (results.length) {
+    var appendString = '';
+
+    for (var i = 0; i < results.length; i++) {
+      var item = store[results[i].ref];
+      appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+      appendString += '<p>' + item.content.substring(0, 150) + ' ...</p></li>';
+    }
+
+    searchResults.innerHTML = appendString;
+  }
+  else {
+    searchResults.innerHTML = '<li>Khong co ket qua nao.</li>';
+  }
+}
